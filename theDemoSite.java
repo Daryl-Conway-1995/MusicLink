c45da34dee3ccbd6a9786c0d559e90acd4721aa1
DarylMConway <Daryl.conway@academytrainee.com>
2018-06-18 16:39:43 +0100

    finished demosite

--- theDemoSite.java
+++ theDemoSite.java
@@ -6,6 +6,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class theDemoSite {
@@ -26,9 +27,31 @@ ChromeDriver myDriver;
 	@Test
 	public void test() {
 		myDriver.manage().window().maximize();
-		String url = "http://thedemosite.co.uk/";
+		String url = "http://thedemosite.co.uk/index.php";
 		myDriver.navigate().to(url);
-		assertEquals("http://thedemosite.co.uk/", myDriver.getCurrentUrl());
+		assertEquals("http://thedemosite.co.uk/index.php", myDriver.getCurrentUrl());
+		WebElement linkElem = myDriver.findElementByLinkText("3. Add a User");
+		linkElem.click();
+		assertEquals("http://thedemosite.co.uk/addauser.php", myDriver.getCurrentUrl());
+		WebElement nameInput = myDriver.findElementByName("username");
+		nameInput.sendKeys("simpleLogin");
+		WebElement passInput =  myDriver.findElementByName("password");
+		passInput.sendKeys("simpleLogin");
+		WebElement buttonAdd =  myDriver.findElementByName("FormsButton2");
+		buttonAdd.submit();
+		WebElement linkElem2 = myDriver.findElementByLinkText("4. Login");
+		linkElem2.click();
+		assertEquals("http://thedemosite.co.uk/login.php", myDriver.getCurrentUrl());
+		WebElement nameInput2 = myDriver.findElementByName("username");
+		nameInput2.sendKeys("simpleLogin");
+		WebElement passInput2 =  myDriver.findElementByName("password");
+		passInput2.sendKeys("simpleLogin");
+		WebElement buttonAdd2 =  myDriver.findElementByName("FormsButton2");
+		buttonAdd2.submit();
+		String message = "/html/body/table/tbody/tr/td[1]/big/blockquote/blockquote/font/center/b";
+		assertEquals("**Successful Login**",myDriver.findElementByXPath(message).getText());
+		
+		
 	}
 
 	@After 
